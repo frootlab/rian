303c6a812297a099e1e2dd06d58be9c5bb86ccf2
fishroot <patrick.michl@gmail.com>
2014-02-20 18:37:12 +0100

    version update

--- package/nemoa/__init__.py
+++ package/nemoa/__init__.py
@@ -5,7 +5,7 @@ from .common.log import *
 import nemoa.common.type as type
 import nemoa.workspace.config, nemoa.plot, nemoa.annotation
 
-__version = ('0.4.75', 'noGPU-20140220')
+__version = ('0.4.76', 'noGPU-20140220')
 initLogger()
 
 def new(**kwargs):
