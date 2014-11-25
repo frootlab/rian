927247eb7c7096eb9439ae8e03fde5e1004a5733
Patrick Michl <patrick.michl@gmail.com>
2014-11-25 08:57:18 +0100

    enhancement: fixed #69

--- package/nemoa/common/console.py
+++ package/nemoa/common/console.py
@@ -5,28 +5,112 @@ __email__   = 'patrick.michl@gmail.com'
 __license__ = 'GPLv3'
 
 import os
-import sys
-import termios
-import fcntl
+
+inkey = None
+
+class _Getch:
+    """Get a character from standard input."""
+    def __init__(self):
+        found = False
+        try:
+            self.impl = _GetchWindows()
+            found = True
+        except ImportError:
+            pass
+        if not found:
+            try:
+                self.impl = _GetchUnix()
+                found = True
+            except ImportError:
+                pass
+        if not found:
+            try:
+                self.impl = _GetchMac()
+                found = True
+            except ImportError:
+                pass
+            
+    def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+    def __init__(self):
+        import sys
+        import termios
+        import tty
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+class _GetchMacCarbon:
+    """
+    A function which returns the current ASCII key that is down;
+    if no ASCII key is down, the null string is returned.  The
+    page http://www.mactech.com/macintosh-c/chap02-1.html was
+    very helpful in figuring out how to do this.
+    """
+    def __init__(self):
+        import Carbon
+
+    def __call__(self):
+        import Carbon
+        if Carbon.Evt.EventAvail(0x0008)[0]==0: # 0x0008 is the keyDownMask
+            return ''
+        else:
+            #
+            # The event contains the following info:
+            # (what,msg,when,where,mod)=Carbon.Evt.GetNextEvent(0x0008)[1]
+            #
+            # The message (msg) contains the ASCII char which is
+            # extracted with the 0x000000FF charCodeMask; this
+            # number is converted to an ASCII character with chr() and
+            # returned
+            #
+            (what,msg,when,where,mod)=Carbon.Evt.GetNextEvent(0x0008)[1]
+            return chr(msg & 0x000000FF)
+
 
 def getch():
-    """Return single character from stdin, or None."""
+    if inkey == None: inkey = _Getch()
+    return inkey()
 
-    fd = sys.stdin.fileno()
-    oldterm = termios.tcgetattr(fd)
-    newattr = termios.tcgetattr(fd)
-    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
-    termios.tcsetattr(fd, termios.TCSANOW, newattr)
-    oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
-    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+#import termios
+#import fcntl
 
-    c = None
+#def getch():
+    #"""Return single character from stdin, or None."""
 
-    try:
-        try: c = sys.stdin.read(1)
-        except IOError: pass
-    finally:
-        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
-        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+    #fd = sys.stdin.fileno()
+    #oldterm = termios.tcgetattr(fd)
+    #newattr = termios.tcgetattr(fd)
+    #newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    #termios.tcsetattr(fd, termios.TCSANOW, newattr)
+    #oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    #fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
 
-    return c
+    #c = None
+
+    #try:
+        #try: c = sys.stdin.read(1)
+        #except IOError: pass
+    #finally:
+        #termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+        #fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+
+    #return c
