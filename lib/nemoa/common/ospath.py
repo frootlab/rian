7403b6fa2832ec0e4daa5bcfeb009a2d9ed3d8e5
Patrick Michl <patrick.michl@gmail.com>
2015-02-19 14:58:50 +0100

    enhancement: added post installation hook for file copiing

--- lib/nemoa/common/ospath.py
+++ lib/nemoa/common/ospath.py
@@ -4,7 +4,37 @@ __author__  = 'Patrick Michl'
 __email__   = 'patrick.michl@gmail.com'
 __license__ = 'GPLv3'
 
-import os
+def basename(path):
+    """Get basename of file.
+
+    Args:
+        path (string): path to file
+
+    Returns:
+        String containing basename of file.
+
+    """
+
+    import os
+
+    filename = os.path.basename(path)
+    filebasename = os.path.splitext(filename)[0].rstrip('.')
+
+    return filebasename
+
+
+
+def cwd():
+    """Get path of current working directory.
+
+    Returns:
+        String containing path of current working directory.
+
+    """
+    
+    import os
+    
+    return os.getcwd() + os.sep
 
 def directory(path):
     """Get directory path of file.
@@ -17,12 +47,47 @@ def directory(path):
 
     """
 
-    filepath = os.path.expanduser(path)
-    filepath = os.path.expandvars(filepath)
-    filepath = os.path.abspath(filepath)
-    filepath = os.path.normpath(filepath)
+    import os
 
-    return os.path.dirname(filepath)
+    if isinstance(path, (tuple, list)):
+        path = os.path.sep.join(path)
+
+    path = os.path.expanduser(path)
+    path = os.path.expandvars(path)
+    path = os.path.abspath(path)
+    path = os.path.normpath(path)
+
+    return os.path.dirname(path)
+
+def fileext(path):
+    """Get extension of file.
+
+    Args:
+        path (string): path to file
+
+    Returns:
+        String containing extension of file.
+
+    """
+
+    import os
+
+    filename = os.path.basename(path)
+    file_ext = os.path.splitext(filename)[1].lstrip('.')
+
+    return file_ext
+
+def home():
+    """Get path to current users home directory.
+    
+    Returns:
+        String containing path of home directory.
+    
+    """
+    
+    import os
+    
+    return os.path.expanduser('~')
 
 def joinpath(directory, name, extension):
     """Get path of file.
@@ -37,6 +102,8 @@ def joinpath(directory, name, extension):
 
     """
 
+    import os
+
     path = '%s%s%s.%s' % (directory, os.sep, name, extension)
     path = os.path.expanduser(path)
     path = os.path.expandvars(path)
@@ -45,43 +112,98 @@ def joinpath(directory, name, extension):
 
     return path
 
-def basename(path):
-    """Get basename of file.
-
+def special(name):
+    """Get paths to special directories.
+    
     Args:
-        path (string): path to file
+        name (string):
 
     Returns:
-        String containing basename of file.
+        String containing path of special directory or dictionary
+        containing all platform specific special directories that
+        are implemented.
 
     """
 
-    filename = os.path.basename(path)
-    filebasename = os.path.splitext(filename)[0].rstrip('.')
+    def _special_folders_linux(name):
+        
+        return True
 
-    return filebasename
+    def _special_folders_windows(name):
+        """
+        
+        Args:
+            name (string):
+        
+        """
 
-def fileext(path):
-    """Get extension of file.
+        try:
+            import winshell
+        except:
+            return False
 
-    Args:
-        path (string): path to file
+        if name in ['Application Data', 'userdata']:
+            return winshell.application_data()
+        elif name == 'Bookmarks':
+            return winshell.bookmarks()
+        elif name in ['Common Application Data', 'shareddata']:
+            return winshell.application_data(True)
+        elif name == 'Common Bookmarks':
+            return winshell.bookmarks(True)
+        elif name == 'Common Desktop':
+            return winshell.desktop(True)
+        elif name == 'Common Programs':
+            return winshell.programs(True)
+        elif name == 'Common Start Menu':
+            return winshell.start_menu(True)
+        elif name == 'Common Startup':
+            return winshell.startup(True)
+        elif name == 'Desktop':
+            return winshell.desktop()
+        elif name == 'My Documents':
+            return winshell.my_documents()
+        elif name == 'Programs':
+            return winshell.programs()
+        elif name == 'Recent':
+            return winshell.recent()
+        elif name == 'SendTo':
+            return winshell.sendto()
+        elif name == 'Start Menu':
+            return winshell.start_menu()
+        elif name == 'Startup':
+            return winshell.startup()
 
-    Returns:
-        String containing extension of file.
+        return False
+    
+    import platform
+    ostype = platform.system().lower()
 
-    """
+    if ostype == 'linux': return _special_folders_linux(name)
+    if ostype == 'windows': return _special_folders_windows(name)
 
-    filename = os.path.basename(path)
-    file_ext = os.path.splitext(filename)[1].lstrip('.')
+    return False
 
-    return file_ext
+def copytree(src, tgt):
+    
+    import glob
+    import os
+    import shutil
 
-def cwd():
-    """Get path of current working derctory.
+    for srcsdir in glob.glob(os.path.join(src, '*')):
+        tgtsdir = os.path.join(tgt, basename(srcsdir))
 
-    Returns:
-        String containing path of current working directory.
+        if os.path.exists(tgtsdir):
+            shutil.rmtree(tgtsdir)
 
-    """
-    return os.getcwd() + os.sep
+        try:
+            shutil.copytree(srcsdir, tgtsdir)
+
+        # directories are the same
+        except shutil.Error as e:
+            print('Directory not copied. Error: %s' % e)
+            
+        # any error saying that the directory doesn't exist
+        except OSError as e:
+            print('Directory not copied. Error: %s' % e)
+    
+    return True
--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ __author__  = 'Patrick Michl'
 __email__   = 'patrick.michl@gmail.com'
 __license__ = 'GPLv3'
 
-import setuptools
-
 def getfile(filepath = None):
     """Get the content from a file."""
 
@@ -58,53 +56,91 @@ def getvars(filepath = None):
     
     return variables
 
-pkg = {
-    'name': 'nemoa',
-    'descfile': 'DESCRIPTION.rst',
-    'libdir': 'lib',
-    'userdata': ('data', 'user'),
-    'sharedata': ('data', 'share'),
-    'keywords': 'dataanalysis classification dbn rbm',
-    'install_requires': ['numpy', 'networkx', 'matplotlib'],
-    'extras_require': {
-        'systemsbiology': ['rpy2'],
-        'test': ['unittest'] },
-    'classifiers': [
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: GPLv3',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4' ],
-    'entry_points': {
-        'console_scripts': ['nemoa = nemoa.scripts.console:main'] } }
+def setup_libs():
 
-# prepare dynamic package variables
-srcfile = (pkg['libdir'], pkg['name'], '__init__.py')
-for key, val in getvars(srcfile).items(): pkg[key] = val
-pkg['long_description'] = getfile(pkg['descfile'])
-pkg['package_dir'] = { '': pkg['libdir'] }
-pkg['packages'] = setuptools.find_packages(pkg['libdir'])
+    import setuptools
 
-# run setup from setuptools
-setuptools.setup(
-    name             = pkg['name'],
-    version          = pkg['version'],
-    description      = pkg['description'],
-    url              = pkg['url'],
-    author           = pkg['author'],
-    author_email     = pkg['email'],
-    license          = pkg['license'],
-    keywords         = pkg['keywords'],
-    package_dir      = pkg['package_dir'],
-    packages         = pkg['packages'],
-    classifiers      = pkg['classifiers'],
-    long_description = pkg['long_description'],
-    install_requires = pkg['install_requires'],
-    extras_require   = pkg['extras_require'],
-    entry_points     = pkg['entry_points'],
-    zip_safe         = False )
+    pkg = {
+        'name': 'nemoa',
+        'descfile': 'DESCRIPTION.rst',
+        'libdir': 'lib',
+        'keywords': 'dataanalysis classification dbn rbm',
+        'install_requires': ['numpy', 'networkx', 'matplotlib'],
+        'extras_require': {
+            'systemsbiology': ['rpy2'],
+            'test': ['unittest'] },
+        'classifiers': [
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Science/Research',
+            'Topic :: Scientific/Engineering',
+            'Operating System :: OS Independent',
+            'License :: OSI Approved :: GPLv3',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4' ],
+        'entry_points': {
+            'console_scripts': ['nemoa = nemoa.scripts.console:main'] }}
+
+    # prepare dynamic package variables
+    srcfile = (pkg['libdir'], pkg['name'], '__init__.py')
+    for key, val in getvars(srcfile).items(): pkg[key] = val
+    pkg['long_description'] = getfile(pkg['descfile'])
+    pkg['package_dir'] = { '': pkg['libdir'] }
+    pkg['packages'] = setuptools.find_packages(pkg['libdir'])
+
+    # run setup from setuptools
+    setuptools.setup(
+        name             = pkg['name'],
+        version          = pkg['version'],
+        description      = pkg['description'],
+        url              = pkg['url'],
+        author           = pkg['author'],
+        author_email     = pkg['email'],
+        license          = pkg['license'],
+        keywords         = pkg['keywords'],
+        package_dir      = pkg['package_dir'],
+        packages         = pkg['packages'],
+        classifiers      = pkg['classifiers'],
+        long_description = pkg['long_description'],
+        install_requires = pkg['install_requires'],
+        extras_require   = pkg['extras_require'],
+        entry_points     = pkg['entry_points'],
+        zip_safe         = False )
+
+    return True
+
+def copy_user_data():
+    
+    import nemoa.common.ospath
+    
+    src_base = getpath(('data', 'user'))
+    tgt_base = nemoa.common.ospath.special('userdata')
+    tgt_base = getpath((tgt_base, 'nemoa', 'workspaces'))
+    
+    nemoa.common.ospath.copytree(src_base, tgt_base)
+    
+    return True
+
+def copy_shared_data():
+    
+    import nemoa.common.ospath
+    
+    src_base = getpath(('data', 'shared'))
+    tgt_base = nemoa.common.ospath.special('shareddata')
+    tgt_base = getpath((tgt_base, 'nemoa', 'workspaces'))
+    
+    nemoa.common.ospath.copytree(src_base, tgt_base)
+    
+    return True
+
+def create_shortcuts():
+    
+    return True
+
+if __name__ == '__main__':
+    setup_libs()
+    copy_user_data()
+    copy_shared_data()
+    create_shortcuts()
