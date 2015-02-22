5fa9fb8e83472d288809cd27dbe259f17ccdd75b
Patrick Michl <patrick.michl@gmail.com>
2015-02-22 23:55:44 +0100

    cleanup: fixed setup.py to build distributions

--- setup.py
+++ setup.py
@@ -5,43 +5,6 @@ __author__  = 'Patrick Michl'
 __email__   = 'patrick.michl@gmail.com'
 __license__ = 'GPLv3'
 
-def copytree(src, tgt):
-
-    import glob
-    import os
-    import shutil
-
-    for srcsdir in glob.glob(os.path.join(src, '*')):
-        tgtsdir = os.path.join(tgt, os.path.basename(srcsdir))
-
-        if os.path.exists(tgtsdir):
-            shutil.rmtree(tgtsdir)
-
-        try:
-            shutil.copytree(srcsdir, tgtsdir)
-
-        # directories are the same
-        except shutil.Error as e:
-            print('Directory not copied. Error: %s' % e)
-
-        # any error saying that the directory doesn't exist
-        except OSError as e:
-            print('Directory not copied. Error: %s' % e)
-
-    return True
-
-def getfile(filepath = None):
-    """Get the content from a file."""
-
-    import codecs
-
-    path = getpath(filepath)
-
-    with codecs.open(path, encoding = 'utf-8') as file_handler:
-        content = file_handler.read()
-
-    return content
-
 def getpath(filepath = None):
     """Get absolute filepath from string or iterable."""
 
@@ -58,32 +21,59 @@ def getpath(filepath = None):
     else:
         raise RuntimeError("Invalid filepath given")
 
-def getvars(filepath = None):
-    """Get all __VARIABLE__ from given file."""
-
-    import io
-    import re
-
-    # get file content
-    path = getpath(filepath)
-    with io.open(path, encoding = 'utf8') as file_handler:
-        content = file_handler.read()
-
-    # parse variables with regular expressions
-    key_regex = """__([a-zA-Z][a-zA-Z0-9]*)__"""
-    val_regex = """['\"]([^'\"]*)['\"]"""
-    regex = r"^[ ]*%s[ ]*=[ ]*%s" % (key_regex, val_regex)
-    variables = {}
-    for match in re.finditer(regex, content, re.M):
-        key = str(match.group(1))
-        val = str(match.group(2))
-        variables[key] = val
-
-    return variables
-
-def setuplibs():
+def install():
+    """Setuptools based installation script."""
 
     import setuptools
+    import setuptools.command.install
+
+    class NemoCustomInstall(setuptools.command.install.install):
+        """Customized setuptools install command."""
+
+        def run(self):
+            setuptools.command.install.install.run(self)
+
+            # run post installation script
+            import os
+            import subprocess
+            import sys
+
+            subprocess.call([sys.executable, __file__, 'postinstall'])
+
+    def getfile(filepath = None):
+        """Get the content from a file."""
+
+        import codecs
+
+        path = getpath(filepath)
+
+        with codecs.open(path, encoding = 'utf-8') as file_handler:
+            content = file_handler.read()
+
+        return content
+
+    def getvars(filepath = None):
+        """Get all __VARIABLE__ from given file."""
+
+        import io
+        import re
+
+        # get file content
+        path = getpath(filepath)
+        with io.open(path, encoding = 'utf8') as file_handler:
+            content = file_handler.read()
+
+        # parse variables with regular expressions
+        key_regex = """__([a-zA-Z][a-zA-Z0-9]*)__"""
+        val_regex = """['\"]([^'\"]*)['\"]"""
+        regex = r"^[ ]*%s[ ]*=[ ]*%s" % (key_regex, val_regex)
+        variables = {}
+        for match in re.finditer(regex, content, re.M):
+            key = str(match.group(1))
+            val = str(match.group(2))
+            variables[key] = val
+
+        return variables
 
     pkg = {
         'name': 'nemoa',
@@ -118,6 +108,7 @@ def setuplibs():
     for key, val in getvars(srcfile).items(): pkg[key] = val
     pkg['long_description'] = getfile(pkg['descfile'])
     pkg['package_dir'] = { '': pkg['libdir'] }
+    pkg['cmdclass'] = {'install': NemoCustomInstall}
     pkg['packages'] = setuptools.find_packages(pkg['libdir'])
 
     # run setup from setuptools
@@ -137,13 +128,42 @@ def setuplibs():
         install_requires = pkg['install_requires'],
         extras_require   = pkg['extras_require'],
         entry_points     = pkg['entry_points'],
+        cmdclass         = pkg['cmdclass'],
         zip_safe         = False )
 
-    return True
-
-def setupdata():
+def postinstall():
+    """Post installation script."""
 
     import appdirs
+
+    def copytree(src, tgt):
+
+        import glob
+        import os
+        import shutil
+
+        print 'copying %s -> %s' % (src, tgt)
+
+        for srcsdir in glob.glob(os.path.join(src, '*')):
+            tgtsdir = os.path.join(tgt, os.path.basename(srcsdir))
+
+            if os.path.exists(tgtsdir):
+                shutil.rmtree(tgtsdir)
+
+            try:
+                shutil.copytree(srcsdir, tgtsdir)
+
+            # directories are the same
+            except shutil.Error as e:
+                print('directory not copied. Error: %s' % e)
+
+            # any error saying that the directory doesn't exist
+            except OSError as e:
+                print('directory not copied. Error: %s' % e)
+
+        return True
+
+    print 'running postinstall'
 
     # copy user workspaces
     user_src_base = getpath(('data', 'user'))
@@ -160,5 +180,10 @@ def setupdata():
     copytree(site_src_base, site_tgt_base)
 
 if __name__ == '__main__':
-    setuplibs()
-    setupdata()
+
+    import sys
+
+    if sys.argv[1] == 'postinstall':
+        postinstall()
+    else:
+        install()
