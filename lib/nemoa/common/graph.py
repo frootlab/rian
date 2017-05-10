improvement: fixed scaling for layergraph plots

lib/nemoa/common/graph.py
@@ -4,8 +4,52 @@ __author__  = 'Patrick Michl'
 __email__   = 'patrick.michl@gmail.com'
 __license__ = 'GPLv3'
 
-def nx_multilayer_layout(graph, minimize = 'weight', threshold = 0.0,
-    transform = None):
+def nx_get_layout(graph, layout = 'spring', scale = None,
+    padding = None, **kwargs):
+    """Calculate positions of nodes, depending on graph layout.
+
+    Args:
+        graph: networkx graph instance
+    
+    Kwargs:
+        layout (string):
+        scale (tuple or None):
+
+    """
+
+    try: import networkx as nx
+    except ImportError: raise ImportError(
+        "nx_layout() requires networkx: https://networkx.github.io/")
+
+    # Todo: allow layouts from pygraphviz_layout
+    
+    if layout == 'spring':
+        pos = nx.spring_layout(graph, **kwargs)
+    elif layout == 'multilayer':
+        pos = nx_get_multilayer_layout(graph, **kwargs)
+    elif layout == 'random':
+        pos = nx.random_layout(graph, **kwargs)
+    elif layout == 'circular':
+        pos = nx.circular_layout(graph, **kwargs)
+    elif layout == 'shell':
+        pos = nx.shell_layout(graph, **kwargs)
+    elif layout == 'spectral':
+        pos = nx.spectral_layout(graph, **kwargs)
+    elif layout == 'fruchterman_reingold':
+        pos = nx.fruchterman_reingold_layout(graph, **kwargs)
+    else:
+        raise ValueError("nx_layout() invalid layout.")
+        return None
+
+    if isinstance(scale, type(None)): return pos
+
+    # rescale node positions to figure size and padding
+    pos = nx_rescale_layout(pos, scale = scale, padding = padding)
+ 
+    return pos
+
+def nx_get_multilayer_layout(graph, direction = 'right',
+    minimize = 'weight'):
     """Calculate positions for layer layout of multilayer graphs.
 
     Args:
@@ -13,18 +57,15 @@ def nx_multilayer_layout(graph, minimize = 'weight', threshold = 0.0,
 
     """
 
-    try: import numpy
+    try: import numpy as np
     except ImportError: raise ImportError(
-        "nx_multilayer_layout() requires numpy: https://scipy.org/")
-    try: import networkx
-    except ImportError: raise ImportError(
-        "nx_multilayer_layout() requires networkx: https://networkx.github.io/")
+        "nx_get_multilayer_layout() requires numpy: https://scipy.org/")
 
     assert nx_is_multilayer(graph), \
-        "nx_multilayer_layout() requires networkx multilayer graph"
+        "nx_get_multilayer_layout() requires networkx multilayer graph"
 
     if len(graph) == 0: return {}
-    if len(graph) == 1: return { graph.nodes()[0]: (0.5, 0.5)}
+    if len(graph) == 1: return { graph.nodes()[0]: (0.5, 0.5) }
 
     # create node stack as list of lists
     layers = graph.graph['params']['layer']
@@ -37,7 +78,8 @@ def nx_multilayer_layout(graph, minimize = 'weight', threshold = 0.0,
         n = data['params']['layer_sub_id']
         stack[l][n] = node
 
-    # sort node stack by attribute
+    # sort node stack to minimize the euclidean distances
+    # of connected nodes
     if isinstance(minimize, str):
         edges = {(u, v): data \
             for (u, v, data) in graph.edges(data = True)}
@@ -48,7 +90,7 @@ def nx_multilayer_layout(graph, minimize = 'weight', threshold = 0.0,
             tlen = len(tgt)
 
             # calculate cost matrix for positions by weights
-            cost = numpy.zeros((tlen, tlen))
+            cost = np.zeros((tlen, tlen))
             for sid, u in enumerate(src):
                 for tid, v in enumerate(tgt):
                     data = edges.get((u, v))
@@ -56,12 +98,9 @@ def nx_multilayer_layout(graph, minimize = 'weight', threshold = 0.0,
                     if not isinstance(data, dict): continue
                     value = data.get(minimize)
                     if not isinstance(value, float): continue
-                    weight = numpy.absolute(value)
-                    if weight < threshold: continue
-                    if type(transform) == 'function':
-                        weight = transform(weight)
+                    weight = np.absolute(value)
                     for pid in range(tlen):
-                        dist = numpy.absolute(
+                        dist = np.absolute(
                             (pid + .5) / (tlen + 1.) \
                             - (sid + .5) / (slen + 1.))
                         cost[pid, tid] += dist * weight
@@ -73,25 +112,66 @@ def nx_multilayer_layout(graph, minimize = 'weight', threshold = 0.0,
             psel = range(tlen) # position select list
             sort = [None] * tlen 
             for i in range(tlen):
-                cmax = numpy.amax(cost[psel][:, nsel], axis = 0)
-                cmin = numpy.amin(cost[psel][:, nsel], axis = 0)
+                cmax = np.amax(cost[psel][:, nsel], axis = 0)
+                cmin = np.amin(cost[psel][:, nsel], axis = 0)
                 diff = cmax ** 2 - cmin ** 2
-                nid  = nsel[numpy.argmax(diff)]
-                pid  = psel[numpy.argmin(cost[psel][:, nid])]
+                nid  = nsel[np.argmax(diff)]
+                pid  = psel[np.argmin(cost[psel][:, nid])]
                 sort[pid] = tgt[nid]
                 nsel.remove(nid)
                 psel.remove(pid)
             stack[lid] = sort
 
     # calculate node positions in box [0, 1] x [0, 1]
+    orientate = lambda (x, y), d: {'right': (x, y), 'up': (y, x),
+        'left': (1. - x,  y), 'down': (y, 1. - x)}.get(d, (x, y))
     pos = {}
     for l, layer in enumerate(stack):
         for n, node in enumerate(layer):
             x = float(l) / (len(stack) - 1)
             y = (float(n) + 0.5) / len(layer)
-            pos[node] = (x, y)
+            pos[node] = orientate((x, y), direction)
 
     return pos
+
+def nx_get_normsizes(pos):
+    """Calculate normal sizes for given node positions.
+
+    Args:
+        graph: networkx graph instance
+
+    """
+
+    try: import numpy as np
+    except ImportError: raise ImportError(
+        "nx_get_normsizes() requires numpy: https://scipy.org/")
+
+    # calculate norm scaling
+    scale = nx_get_normscale(pos)
+    
+    return {
+        'node_size':   0.0558 * scale ** 2,
+        'node_radius': 23. * (0.01 * scale - 0.2),
+        'line_width':  0.0030 * scale,
+        'font_size':  0.1200 * scale }
+
+def nx_get_groups(graph, param = None):
+
+    # get groups
+    groups = {None: []}
+    for node, data in graph.nodes(data = True):
+        if not isinstance(data, dict) \
+            or not 'params' in data \
+            or not param in data['params']:
+            groups[None].append(node)
+            continue
+        group = data['params'].get(param)
+        if not group in groups:
+            groups[group] = [node]
+            continue
+        groups[group].append(node)
+    
+    return groups
 
 def nx_is_multilayer(graph):
     """Test if graph is multilayer graph.
@@ -110,45 +190,38 @@ def nx_is_multilayer(graph):
 
     return retval
 
-def nx_get_scaling(pos):
+def nx_get_normscale(pos):
     """Calculate scaling.
 
     Args:
-        pos:
+        pos: dictionary with node positions
 
     """
 
-    try: import numpy
+    try: import numpy as np
     except ImportError: raise ImportError(
-        "nx_get_scaling() requires numpy: https://scipy.org/")
+        "nx_get_normscale() requires numpy: https://scipy.org/")
 
-    # calculate euclidean distances of node positions
-    dist = lambda (ux, uy), (vx, vy): \
-        numpy.sqrt((ux - vx) ** 2 + (uy - vy) ** 2)
-    #darr = numpy.zeros((len(pos), len(pos)))
-    dseq = []
+    # calculate euclidean distances between node positions
+    euklid = lambda (a, b), (c, d): np.sqrt((a - c) ** 2 + (b - d) ** 2)
+    dlist = []
     for i, u in enumerate(pos.keys()):
         for j, v in enumerate(pos.keys()[i + 1:], i + 1):
-            dseq.append(dist(pos[u], pos[v]))
-            #d = dist(pos[u], pos[v])
-            #darr[i, j] = darr[j, i] = d
-            #dseq.append(d)
-    darr = numpy.array(dseq)
+            dlist.append(euklid(pos[u], pos[v]))
+    darr = np.array(dlist)
 
-    # calculate maximal scaling factor from minimal node distance
-    maxscale = 2.32 * numpy.amin(darr)
-
-    # calculate minimal scaling factor from average node distance
-    minscale = 0.2 * numpy.mean(darr)
+    # calculate maximal scaling factor from minimal node distance and
+    # minimal scaling factor from average node distance
+    maxscale = 2.32 * np.amin(darr)
+    minscale = 0.2 * np.mean(darr)
 
     return (minscale + maxscale) / 2
 
-def nx_rescale_layout(pos, xscale = (0., 1.), yscale = (0., 1.),
-        padding = (0., 0., 0., 0.), rotate = 0.):
+def nx_rescale_layout(pos, scale = None, padding = None, rotate = 0.):
     """Rescale node positions.
 
     Args:
-        pos:
+        pos: dictionary with node positions
     
     Kwargs:
         xscale:
@@ -158,31 +231,27 @@ def nx_rescale_layout(pos, xscale = (0., 1.), yscale = (0., 1.),
 
     """
 
-    try: import numpy
+    try: import numpy as np
     except ImportError: raise ImportError(
         "nx_rescale_layout() requires numpy: https://scipy.org/")
 
-    nodes = pos.keys()
-    data = numpy.array([[x, y] for x, y in pos.values()])
-    size = numpy.array([xscale, yscale])
+    data = np.array([(x, y) for x, y in pos.values()])
 
     # rotate positions around its center a by given rotation angle
     if bool(rotate):
-        theta = numpy.radians(rotate)
-        cos, sin = numpy.cos(theta), numpy.sin(theta)
-        R = numpy.array([[cos, -sin], [sin, cos]])
+        theta = np.radians(rotate)
+        cos, sin = np.cos(theta), np.sin(theta)
+        R = np.array([[cos, -sin], [sin, cos]])
         mean = data.mean(axis = 0)
-        data = numpy.dot(data - mean, R.T) + mean
+        data = np.dot(data - mean, R.T) + mean
 
-    # rescale position to box [0, 1] x [0, 1] with padding [u, r, d, l]
-    dmin, dmax = numpy.amin(data, axis = 0), numpy.amax(data, axis = 0)
-    u, r, d, l = padding
-    pmin, pmax = numpy.array([l, d]), 1. - numpy.array([r, u])
-    data = (pmax - pmin) * (data - dmin) / (dmax - dmin) + pmin
+    # rescale positions with padding [u, r, d, l]
+    if not isinstance(scale, type(None)):
+        dmin, dmax = np.amin(data, axis = 0), np.amax(data, axis = 0)
+        if isinstance(padding, type(None)): u, r, d, l = 0., 0., 0., 0.
+        else: u, r, d, l = padding
+        pmin, pmax = np.array([l, d]), 1. - np.array([r, u])
+        data = (pmax - pmin) * (data - dmin) / (dmax - dmin) + pmin
+        data = np.array(scale) * data
 
-    # rescale box to given xscale and yscale
-    sarr = numpy.array([xscale, yscale]).T
-    smin, smax = sarr[0], sarr[1]
-    data = (smax - smin) * data + smin
-
-    return {node: tuple(data[i]) for i, node in enumerate(nodes)}
+    return {node: tuple(data[i]) for i, node in enumerate(pos.keys())}

lib/nemoa/common/plot.py
@@ -114,7 +114,6 @@ def graph(graph, **kwargs):
     node_size_scale = 1.85   # node size scale factor
     font_size_max = 18.      # maximum font size
     edge_size_scale = 4.     # edge size scale factor
-    edge_normalize = True
     edge_arr_scale = 8.      # edge arrow size scale factor
     edge_radius = 0.15       # edge radius for fancy edges
 
@@ -182,12 +181,6 @@ def graph(graph, **kwargs):
         graph.node[node]['patch'] = c
 
     # draw edges
-    if edge_normalize:
-        max_weight = 0.0
-        for (u, v, attr) in graph.edges(data = True):
-            if max_weight > attr['weight']: continue
-            max_weight = attr['weight']
-        edge_line_width /= max_weight
     seen = {}
     for (u, v, attr) in graph.edges(data = True):
         n1 = graph.node[u]['patch']
@@ -241,7 +234,7 @@ def layergraph(graph, **kwargs):
         edge_curvature (float): value within the intervall [-1, 1],
             that determines the curvature of the edges.
             Thereby 1 equals max convexity and -1 max concavity.
-        graph_direction (string): string within the list ['up', 'down',
+        direction (string): string within the list ['up', 'down',
             'left', 'right'], that dermines the plot direction of the
             graph. 'up' means, the first layer is at the bottom.
         edge_style (string):  '-', '<-', '<->', '->',
@@ -253,10 +246,12 @@ def layergraph(graph, **kwargs):
 
     """
 
-    try: import numpy
+    try:
+        import numpy
     except ImportError: raise ImportError(
         "layergraph() requires numpy: https://scipy.org/")
-    try: import networkx
+    try:
+        import networkx as nx
     except ImportError: raise ImportError(
         "layergraph() requires networkx: https://networkx.github.io/")
     try:
@@ -264,15 +259,22 @@ def layergraph(graph, **kwargs):
         import matplotlib.patches
     except ImportError: raise ImportError(
         "layergraph() requires matplotlib: https://matplotlib.org/")
-
-    import nemoa.common.dict
-    import nemoa.common.text
-    import nemoa.common.math
+    try:
+        import nemoa.common.graph as nmgraph
+        import nemoa.common.dict as nmdict
+        import nemoa.common.text as nmtext
+    except ImportError: raise ImportError(
+        "layergraph() requires nemoa: https://github.com/fishroot/nemoa/")
 
     # default settings
-    kwargs = nemoa.common.dict.merge(kwargs, {
-        'layout':               'random',
-        'graph_direction':      'right',
+    kwargs = nmdict.merge(kwargs, {
+        'layout':               'multilayer',
+        'layout_params':        {},
+        'show_legend':          False
+        'legend_fontsize':      9.0,
+        'show_title':           False,
+        'title_fontsize':       16.,
+        'direction':            'right',
         'figure_padding':       (0.1, 0.1, 0.1, 0.1),
         'figure_size':          (6.4, 4.8), # (11.69,8.27) for A4
                                             # (16.53,11.69) for A3
@@ -280,22 +282,18 @@ def layergraph(graph, **kwargs):
         'node_groupby':         'visible',
         'node_groups': {
             True: {
-                'label': 'Observable',
+                'label': 'Observable Variable',
                 'bg_color': 'marine blue',
                 'font_color': 'white',
                 'border_color': 'dark navy'},
             False: {
-                'label': 'Latent',
+                'label': 'Latent Variable',
                 'bg_color': 'light grey',
                 'font_color': 'dark grey',
                 'border_color': 'grey'} },
-        'node_sort':            True,
-        'node_scale':           1.0,
-        'node_fontsize':        16.0,
         'node_color':           True,
         'edge_style':           '-|>',
         'edge_attribute':       'weight',
-        'edge_threshold':       0.75,
         'edge_scale':           1.0,
         'edge_arrow_scale':     12.0,
         'edge_width_enabled':   True,
@@ -315,55 +313,24 @@ def layergraph(graph, **kwargs):
     ax.set_aspect('equal', 'box')
     ax.axis('off')
 
-    # get node positions in box [0, 1] x [0, 1]
-    # Todo: allow layouts from pygraphviz_layout
-    layout = kwargs.get('layout', 'spring').lower()
-    if layout == 'spring': pos = networkx.spring_layout(graph)
-    elif layout == 'multilayer':
-        pos = nemoa.common.graph.nx_multilayer_layout(graph,
-            minimize = kwargs.get('edge_attribute') \
-                if kwargs.get('node_sort') else None, 
-            threshold = kwargs.get('edge_threshold'),
-            transform = nemoa.common.math.softstep)
-    elif layout == 'random':
-        pos = networkx.random_layout(graph)
-    elif layout == 'circular':
-        pos = networkx.circular_layout(graph)
-    elif layout == 'shell':
-        pos = networkx.shell_layout(graph)
-    elif layout == 'spectral':
-        pos = networkx.spectral_layout(graph)
-    elif layout == 'fruchterman_reingold':
-        pos = network.fruchterman_reingold_layout(graph)
-    else: pos = networkx.spring_layout(graph)
+    # get node positions
+    pos = nmgraph.nx_get_layout(graph,
+        layout = kwargs.get('layout', 'spring').lower(),
+        scale = figsize,
+        padding = kwargs.get('figure_padding', None),
+        **kwargs.get('layout_params', {}))
 
-    # rescale node positions to figure size and padding
-    angle = {'right': 0, 'up': 90, 'left': 180, 'down': 270}.get(
-        kwargs.get('graph_direction', 'right'), 0)
-    pos = nemoa.common.graph.nx_rescale_layout(pos,
-        xscale = (0., figsize[0]), yscale = (0., figsize[1]),
-        padding = kwargs.get('figure_padding'), rotate = angle)
+    # get normal sizes with respect to node positions
+    sizes = nmgraph.nx_get_normsizes(pos)
+    node_size = sizes.get('node_size', None)
+    node_radius = sizes.get('node_radius', None)
+    line_width = sizes.get('line_width', None)
+    font_size = sizes.get('font_size', None)
 
-    # calculate scaling of nodes and edges from node positions
-    scale = nemoa.common.graph.nx_get_scaling(pos) \
-        * kwargs.get('node_scale', 1.0)
-    node_size = 0.0558 * scale ** 2
-    line_width = 0.0030 * scale
-    fontsize = 0.0075 * scale * kwargs.get('node_fontsize', 16.0)
-    nradius = 23 * (0.01 * scale - 0.2)
-
-    # get groups
-    groups = {None: []}
-    attr = kwargs['node_groupby']
-    nodes = {n: data for (n, data) in graph.nodes(data = True)}
-    for node, data in nodes.iteritems():
-        if not isinstance(data, dict) \
-            or not 'params' in data or not attr in data['params']:
-            groups[None].append(node)
-            continue
-        if not data['params'][attr] in groups:
-            groups[data['params'][attr]] = []
-        groups[data['params'][attr]].append(node)
+    # get nodes and groups
+    nodes = {n: data for n, data in graph.nodes(data = True)}
+    groups = nmgraph.nx_get_groups(graph,
+        param = kwargs.get('node_groupby', None))
 
     # draw nodes, labeled by groups
     for group in sorted(groups):
@@ -386,7 +353,7 @@ def layergraph(graph, **kwargs):
         nodes_in_group = groups[group]
 
         # draw nodes in group
-        node_obj = networkx.draw_networkx_nodes(graph, pos,
+        node_obj = nx.draw_networkx_nodes(graph, pos,
             nodelist   = nodes_in_group,
             linewidths = line_width,
             node_size  = node_size,
@@ -400,21 +367,20 @@ def layergraph(graph, **kwargs):
 
             # determine label and fontsize
             label_str = nodes[node]['params']['label']
-            node_label = nemoa.common.text.labelfomat(label_str)
-            node_font_size = fontsize / len(label_str.rstrip('1234567890'))
-            # 2do
+            node_label = nmtext.labelfomat(label_str)
+            node_label_size = len(label_str.rstrip('1234567890'))
 
             # draw node label
-            networkx.draw_networkx_labels(graph, pos,
+            nx.draw_networkx_labels(graph, pos,
                 labels      = {node: node_label},
-                font_size   = node_font_size,
+                font_size   = font_size / node_label_size,
                 font_color  = font_color,
                 font_family = 'sans-serif',
                 font_weight = 'normal')
 
             # patch node for edges
-            c = matplotlib.patches.Circle(pos[node],
-                radius = nradius, alpha = 0.)
+            c = matplotlib.patches.Circle(pos.get(node),
+                radius = node_radius, alpha = 0.)
             ax.add_patch(c)
             graph.node[node]['patch'] = c
 
@@ -433,17 +399,16 @@ def layergraph(graph, **kwargs):
             val = data.get(attr)
             if not isinstance(val, float): continue
             weight = numpy.absolute(val)
-            if weight < kwargs['edge_threshold']: continue
 
         # calculate edge curvature from node positions
         if (u, v) in seen:
             rad = seen.get((u, v))
             rad = -(rad + float(numpy.sign(rad)) * .2)
-        elif kwargs['graph_direction'] in ['top', 'down']:
+        elif kwargs['direction'] in ['top', 'down']:
             rad = 2.0 * kwargs['edge_curvature'] \
                 * (pos[u][1] - pos[v][1]) * (pos[u][0] - pos[v][0]) \
                 / figsize[0] / figsize[1]
-        elif kwargs['graph_direction'] in ['right', 'left']:
+        elif kwargs['direction'] in ['right', 'left']:
             rad = -2.0 * kwargs['edge_curvature'] \
                 * (pos[u][1] - pos[v][1]) * (pos[u][0] - pos[v][0]) \
                 / figsize[0] / figsize[1]
@@ -480,5 +445,23 @@ def layergraph(graph, **kwargs):
             color           = color(edge_color, 'black'),
             alpha           = numpy.amin([weight, 1.0]) )
         ax.add_patch(arrow)
+
+    # (optional) draw legend
+    if bool(kwargs.get('show_legend', False)):
+        legend_fontsize = kwargs.get('legend_fontsize', 9.)
+        ax.legend(
+            numpoints      = 1,
+            loc            = 'lower left',
+            ncol           = 4,
+            borderaxespad  = 0.,
+            bbox_to_anchor = (0., -0.1),
+            fontsize       = legend_fontsize,
+            markerscale    = 0.6 * legend_fontsize / font_size)
+
+    # (optional) draw title
+    if bool(kwargs.get('show_title', False)):
+        title_fontsize = kwargs.get('title_fontsize', 16.)
+        matplotlib.pyplot.title(kwargs.get('title', 'Unknown'),
+            fontsize = title_fontsize)
 
     return True