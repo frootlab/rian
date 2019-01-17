a3ea2a411d1503c26e76217c30ceebe8e6085cdc
fishroot <patrick.michl@gmail.com>
2019-01-17 11:33:49 +0100

    fixes #637

--- nemoa/base/abc.py
+++ nemoa/base/abc.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Metaclasses and Abstract Base Classes for frequently used design patterns."""
+"""Metaclasses and Abstract Base Classes for frequently used patterns."""
 
 __author__ = 'Patrick Michl'
 __email__ = 'frootlab@gmail.com'
@@ -18,36 +18,36 @@ class SingletonMeta(ABCMeta):
 
     Singleton classes only create a single instance per application. This
     creation behaviour is comparable to global variables and used to ensure the
-    application global uniqueness and accessibility of an object. Usage examples
-    include logging, sentinel objects and application global configurations.
+    application global uniqueness and accessibility of an object. Common use
+    cases for Singletons comprise logging, sentinel objects and application
+    global constants (given as immutable objects).
 
     """
-    _registry: Dict[type, object] = {}
+    _instance: Optional[type] = None
 
     def __call__(cls, *args: Any, **kwds: Any) -> object:
-        try:
-            obj = cls._registry[cls]
-        except KeyError:
-            obj = super(SingletonMeta, cls).__call__(*args, **kwds)
-            cls._registry[cls] = obj
-        return obj
-
-    @classmethod
-    def __instancecheck__(mcs, obj: object) -> bool:
-        if obj.__class__ is mcs:
-            return True
-        return isinstance(obj.__class__, mcs)
+        if not cls._instance:
+            cls._instance = super(SingletonMeta, cls).__call__(*args, **kwds)
+        return cls._instance
 
 class Singleton(metaclass=SingletonMeta):
-    """Base Class for Singleton Classes."""
+    """Base Class for Singleton Classes.
+
+    The usage of a Singleton base class allows the usage if :func:`isinstance`
+    to check the type of an object against Singleton.
+
+    """
     __slots__: list = []
 
 class MultitonMeta(ABCMeta):
     """Metaclass for Multiton Classes.
 
-    Moltiton Classes allow the controlled creation of multiple instances, by
-    comparing the given arguments and keyword arguments with entries in a
-    registry.
+    Multiton Classes only create a single instance per given arguments. This
+    allows a controlled creation of multiple objects, that are globally
+    accessible and unique. Multiton classes may be regarded as generalizations
+    of Singleton Classes in the sense of a 'Registry of Singletons'. Common
+    use cases comprise application global configurations, caching and
+    collections of constants (given as immutable objects).
 
     """
     _registry: Dict[Tuple[type, tuple, Any], object] = {}
@@ -55,11 +55,7 @@ class MultitonMeta(ABCMeta):
     def __call__(cls, *args: Any, **kwds: Any) -> object:
         # Create 'fingerprint' of instance. Beware: The fingerprint is only
         # hashable if all given arguments and keywords are hashable
-        try:
-            key = (cls, args, frozenset(kwds.items()))
-        except TypeError:
-            print(cls, args, kwds) # TODO
-            raise
+        key = (cls, args, frozenset(kwds.items()))
 
         # Check registry for the fingerprint. If the fingerprint is not hashable
         # create and return and an instance of the class. If the the fingerprint
@@ -72,15 +68,16 @@ class MultitonMeta(ABCMeta):
         except KeyError:
             hashable = True
 
-        # Create an instance of the class. (Note, that if the class does not
-        # implement a __init__ method, then it does not accept arguments)
+        # Create an instance of the class. Note, that if the class does not
+        # implement a __init__ method, then it does not accept arguments. In
+        # this case a TypeError is raised.
         try:
             obj = super(MultitonMeta, cls).__call__(*args, **kwds)
         except TypeError:
             obj = super(MultitonMeta, cls).__call__()
 
-        # If the fingerprint is hashable, add an entry to the registry and
-        # finally return the instance.
+        # If the fingerprint is hashable, add the instance to the registry and
+        # finally return it.
         if hashable:
             cls._registry[key] = obj
         return obj
